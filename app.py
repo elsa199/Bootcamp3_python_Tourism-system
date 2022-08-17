d114b346c20c3f600e7daf9c69dafe5cca88b1d8
Hamed Molavi <hamed.molavi1998@gmail.com>
2022-08-17 16:04:35 +0430

    Debugging all of the modules

--- app.py
+++ app.py
@@ -16,11 +16,13 @@ start = inp(
     key=lambda el: el > 0 and el <= len(cities)
 )
 
-indexes = inp(
+trip = inp(
     'Please enter your visiting cities as a sequence (e.g. 1 2 3): ',
     f'Please enter a valid sequence of numbers between 1 and {len(cities)}, also dont enter your start city as first destination: ',
     convert=lambda Ls: [int(el) for el in Ls.split()],
     key=lambda L: L[0] != start
 )
 
-[indexes, best_distance] = ga(cities, distances, indexes, start)
+[trip, best_distance] = ga(cities, distances, trip, start)
+
+print(trip, best_distance)
--- tools/ga.py
+++ tools/ga.py
@@ -1,25 +1,36 @@
+import sys
 import numpy as np
 from numpy import array as Array
 from sko.GA import GA_TSP as __GA_TSP
 from tools.swap import swap
+from sko.tools import set_run_mode
 
 
 
 def ga(cities: Array, distances: list, indexes: list, start: int) -> list:
     no_cities = len(cities)
+    start -= 1
     for el in indexes: #                        Check user numbers
         if el < 0 or el > no_cities:
             raise Exception()
+    
+    no_points = len(indexes)
+    cut_distances = np.zeros([no_points, no_points])
+    for indi, i in enumerate(indexes):
+        for indj, j in enumerate(indexes):
+            cut_distances[indi][indj] = distances[i - 1][j - 1]
+
             
     def distance(routine):
         '''The objective function. input routine, return total distance.
         distance(np.arange(num_points))
         '''
-        routine = np.insert(routine, 0, start)
+        # routine = np.insert(routine, 0, start) [1, 2, 0]
         num_points, = routine.shape
-        d = [distances[routine[i - 1]][routine[i]] for i in range(1, num_points)]
-        return sum(d)
-
+        d = distances[start][indexes[routine[0]] - 1] # Distance from start to first city in route
+        d += sum([cut_distances[routine[i - 1]][routine[i]] for i in range(1, num_points)]) # Distance of the rest of the route
+        return d
+    # set_run_mode(distance, 'vectorization')
     ga_tsp = __GA_TSP(func=distance, n_dim=len(indexes), size_pop=50, max_iter=500, prob_mut=1)
     arrange, best_distance = ga_tsp.run()
     indexes = swap(indexes, arrange)
