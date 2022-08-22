3ffad34ca98eb2abad3136fa542d1e46aaa97cc5
elsa199 <master95.ai.kazemi@gmail.com>
2022-08-22 18:35:55 +0430

    v3

--- app.py
+++ app.py
@@ -3,12 +3,20 @@ from packages.tourist.Class import Tourist
 from packages.admin.Class import Admin
 from packages.utils.env import main as initialize
 from packages.utils.login import login
+from loguru import logger
+import sys
+
 clear_console(0)
 
+logger.remove()
+logger.add(sys.stderr, format = "{time:HH:mm:ss} | <red>[{level}]</red> Message : <green>{message}</green>", colorize=True)
+logger.info("The program was started successfully")
 print('---------------------------------------------------------------------')
 print('                     Wellcome to our program                         ')
-print('         When ever you wanted to cancel use-> Ctrl + c + Enter       ')
+print('         When ever you wanted to cancel use-> Ctrl + C + Enter      ')
 print('---------------------------------------------------------------------\n')
+
+
 rerun = False
 while True:
     inputs = initialize(rerun)
